7e4e1269be1c992613b955f7d2b1a229641b5ab4
Juansu01 <thejuank0@hotmail.com>
2022-06-06 15:21:15 -0500

    Add advanced task

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,6 @@ This module defines the function safe_first_element.
 from typing import Any, Sequence, Union
 
 
-# The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     Returns the first element.
