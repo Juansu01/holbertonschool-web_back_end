619caa37ab972b9c4bdff776c40b2fae93d69a04
Juansu01 <thejuank0@hotmail.com>
2022-06-06 13:59:16 -0500

    Add missing types

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,7 +7,7 @@ This module defines the to_kv function.
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple, the first element is k and the second
     element is the square of the int/float.
