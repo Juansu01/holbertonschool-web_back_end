0fa16561630744d7bd6d23afaca12ce78f465679
Juansu01 <thejuank0@hotmail.com>
2022-06-07 12:11:35 -0500

    Add: Tasks 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""This module defines the task_wait_n routine."""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Returns a list of all delays in
+    ascending order.
+    """
+
+    delays = []
+    delays_asc = []
+
+    for i in range(n):
+        delays.append(task_wait_random(max_delay))
+
+    for f in asyncio.as_completed(delays):
+        delays_asc.append(await f)
+
+    return delays_asc
