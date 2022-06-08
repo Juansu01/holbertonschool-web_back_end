611676ce596eb2acc1d00cd9e9857000bb6eaa6e
Juansu01 <thejuank0@hotmail.com>
2022-06-08 15:57:18 -0500

    Reverting

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Loops 10 times and yields a random number
     between 0 and 10.
