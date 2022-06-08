8db1697fcb3e06168f4d207a2dc103c18d7717de
Juansu01 <thejuank0@hotmail.com>
2022-06-08 16:07:54 -0500

    Add: Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module defines the async_comprehension coroutine.
+"""
+
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Async list comprenhension.
+    """
+    return [i async for i in async_generator()]
