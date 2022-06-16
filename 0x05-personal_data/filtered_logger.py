fe9c4f1852a031e4545c57fd92d11283dd90547a
Juansu01 <thejuank0@hotmail.com>
2022-06-16 15:32:05 -0500

    Add: Log formatter

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,6 +6,32 @@ filter_datum module.
 import re
 from typing import List
 
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        This method filters values in incoming log records
+        using filter_datum.
+        """
+
+        filtered = filter_datum(self.fields, self.REDACTION,
+                                super().format(record), self.SEPARATOR)
+
+        return filtered
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
