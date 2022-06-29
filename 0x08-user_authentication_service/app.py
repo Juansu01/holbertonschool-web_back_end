88ff18ca4e51515b93660df551c10f1978d6b099
Juansu01 <thejuank0@hotmail.com>
2022-06-29 14:11:04 -0500

    Add: Log in

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,7 +4,8 @@ This module defines the routes for our API.
 """
 
 
-from flask import Flask, jsonify, request
+from urllib import response
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -17,7 +18,6 @@ def welcome_message() -> str:
     """
     First route for API.
     """
-
     return jsonify({"message": "Bienvenue"})
 
 
@@ -35,6 +35,30 @@ def register_user() -> str:
         return jsonify({"email": email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """
+    API login endpoint.
+    """
+
+    try:
+        email, pws = request.form["email"], request.form["password"]
+    except KeyError:
+        abort(400)
+
+    if AUTH.valid_login(email, pws):
+        sess_id = AUTH.create_session(email)
+        res = jsonify({
+            "email": email,
+            "message": "logged in"
+        })
+        response.set_cookie("session_id", sess_id)
+        return res
+
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
