b9f4212b6a8515567b1c1e4afdeb3a1c0339092c
Juansu01 <thejuank0@hotmail.com>
2022-07-02 11:53:22 -0500

    Trying to fix

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -59,15 +59,15 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None and\
-            auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
     current_user = auth.current_user(request)
-    if current_user:
-        request.current_user = current_user
-    else:
+    if current_user is None:
         abort(403)
+
+    request.current_user = current_user
 
 
 if __name__ == "__main__":
