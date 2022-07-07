3f2e7abf63064e08c255dbd0e4ed19aaaec86ee5
Juansu01 <thejuank0@hotmail.com>
2022-07-07 14:58:14 -0500

    Add: Get locale from request

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Module for i18n project.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    Configuration for Babel.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home() -> str:
+    """
+    Home route rendering simple html.
+    """
+    return render_template("0-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determines the best match with our supported languages
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run()
