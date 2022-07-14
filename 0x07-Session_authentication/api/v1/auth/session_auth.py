214a1d0fae4dab3a5a052aa58dc4d1739efecb3c
Juansu01 <thejuank0@hotmail.com>
2022-07-14 17:01:56 -0500

    Add: Empty session

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module defines the auth class.
+"""
+from flask import request
+from typing import List, TypeVar
+import re
+import os
+
+
+class SessionAuth:
+    """
+    This is the Auth class that manages
+    API authentication.
+    """
+    pass
