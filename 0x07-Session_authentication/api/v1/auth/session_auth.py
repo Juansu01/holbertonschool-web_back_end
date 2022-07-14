2dafa5a108cdd0b035da4f37b9b38e5ef77dbec4
Juansu01 <thejuank0@hotmail.com>
2022-07-14 17:03:24 -0500

    Inheritance from auth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -6,9 +6,10 @@ from flask import request
 from typing import List, TypeVar
 import re
 import os
+from api.v1.auth.auth import Auth
 
 
-class SessionAuth:
+class SessionAuth(Auth):
     """
     This is the Auth class that manages
     API authentication.
