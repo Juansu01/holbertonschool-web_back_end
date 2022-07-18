7280aad91a2a6abc74ffe907b9cc5ef41ca9c4e4
Juansu01 <thejuank0@hotmail.com>
2022-07-18 16:19:26 -0500

    Add: Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Module for Redis basic project.
 """
 
 import redis
-from typing import Union
+from typing import Union, Optional
 import uuid
 
 
@@ -29,3 +29,32 @@ class Cache():
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float]:
+        """
+        Converts the data back to the desired format
+        """
+        value = self._redis.get(key)
+
+        if fn:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Parameterizes value to str.
+        """
+        value = self._redis.get(key).decode("utf-8")
+        return value
+
+    def get_int(self, key: str) -> str:
+        """
+        Parameterizes value to int.
+        """
+
+        try:
+            return int(self._redis.get(key).decode("utf-8"))
+        except Exception:
+            return 0
