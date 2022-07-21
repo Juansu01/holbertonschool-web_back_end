fac4b55c21cedc1d7d399e5ae5014ac0d2596ac4
Juansu01 <thejuank0@hotmail.com>
2022-07-21 16:05:18 -0500

    Add file

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,3 @@
 -- Old school band
 -- Task 3
 
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
