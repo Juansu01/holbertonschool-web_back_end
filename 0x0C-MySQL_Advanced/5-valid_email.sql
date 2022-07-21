d4c8a4aad78799c404d6eb822b4d6a07c3ff76ea
Juansu01 <thejuank0@hotmail.com>
2022-07-21 16:15:01 -0500

    Add: Email validation to sent

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- Email validation to sent
+-- Task 5
+
+DELIMITER $$
+CREATE TRIGGER email_validation
+BEFORE UPDATE
+ON users FOR EACH ROW
+BEGIN
+IF NEW.email <> OLD.email
+THEN
+    SET NEW.valid_email = 0;
+END IF;
+END
+$$
+DELIMITER;
