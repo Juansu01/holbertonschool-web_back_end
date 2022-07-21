8b88b1679a14e0555b3e0724f60e6e5bfaf45ce1
Juansu01 <thejuank0@hotmail.com>
2022-07-21 15:43:34 -0500

    Add: We are all unique!

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- We are all unique!
+-- Task 0
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
