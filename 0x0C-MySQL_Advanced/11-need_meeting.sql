06cb57bf664fc08736060450ee48eb0a4b82369b
Juansu01 <thejuank0@hotmail.com>
2022-07-21 16:30:54 -0500

    Add: No table for a meeting

---
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- No table for a meeting
+-- Task 11
+
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS SELECT name FROM students
+WHERE score < 80 AND (last_meeting IS NULL
+OR
+last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
