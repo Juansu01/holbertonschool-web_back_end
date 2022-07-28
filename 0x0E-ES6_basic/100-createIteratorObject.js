7ae22ee2786489648d1f72f7bcdacef070068a8f
Juansu01 <thejuank0@hotmail.com>
2022-07-28 17:20:46 -0500

    Add advanced task

---
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,23 @@
+export default function createIteratorObject(report) {
+  let iterable = [];
+
+  if (!report.allEmployees) {
+    return iterable;
+  }
+
+  if (typeof report.allEmployees !== "object") {
+    return iterable;
+  }
+
+  iterable = {
+    * [Symbol.iterator]() {
+      for (const value of Object.values(report.allEmployees)) {
+        for (const i of value) {
+          yield i;
+        }
+      }
+    },
+  };
+
+  return iterable;
+}
