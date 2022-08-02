fd30344c91123c01d1b6b5e8bb546c4606a0927a
Juansu01 <thejuank0@hotmail.com>
2022-08-02 17:25:53 -0500

    Add advanced task

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (err) {
+    photo = null;
+    user = null;
+  }
+
+  const uploadUser = { photo, user };
+
+  return uploadUser;
+}
