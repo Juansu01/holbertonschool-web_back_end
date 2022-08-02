7cdbd3da36bf0cd091a51f08fe691d183989d677
Juansu01 <thejuank0@hotmail.com>
2022-08-02 17:00:54 -0500

    Remove extra spaces

--- 0x0F-ES6_promise/0-promise.js
+++ 0x0F-ES6_promise/0-promise.js
@@ -1,12 +1,10 @@
 function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-
         if (true) {
             resolve('True');
         } else {
             reject(new Error('Error'));
         }
-
     });
 }
 
