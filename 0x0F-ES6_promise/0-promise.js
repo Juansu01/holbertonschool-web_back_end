336f17ae60ea9776db0c9b7ed4db4b2c6b654671
Juansu01 <thejuank0@hotmail.com>
2022-08-02 16:46:16 -0500

    Another test

--- 0x0F-ES6_promise/0-promise.js
+++ 0x0F-ES6_promise/0-promise.js
@@ -1,12 +1,12 @@
 function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-        /* eslint-disable */
+
         if (true) {
             resolve('True');
         } else {
             reject(new Error('Error'));
         }
-        /* eslint-enable */
+
     });
 }
 
