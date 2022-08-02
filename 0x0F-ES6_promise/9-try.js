d6cfbd31fe3be92648d03c40c369700d261c364f
Juansu01 <thejuank0@hotmail.com>
2022-08-02 17:17:47 -0500

    Add task 9

---
+++ 0x0F-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+function guardrail(mathFunction) {
+  let res;
+  const queue = [];
+
+  try {
+    res = mathFunction();
+  } catch (err) {
+    res = err.toString();
+  }
+
+  queue.push(res);
+  queue.push('Guardrail was processed');
+  return queue;
+}
