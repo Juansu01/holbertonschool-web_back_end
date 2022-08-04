2e81381ee9eb3a2955841c457c320cb7d9a7cfa3
Juansu01 <thejuank0@hotmail.com>
2022-08-04 17:10:32 -0500

    Add building

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
+
+export default Building;
