4ac891a3aeb2769e69ad5199625ae5ddd2b35e67
Juansu01 <thejuank0@hotmail.com>
2022-08-15 15:35:17 -0500

    Fix?

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  let chunk;
-  // Use a loop to make sure we read all available data.
-  while ((chunk = process.stdin.read()) !== null) {
-   process.stdout.write(`Your name is: ${chunk}`);
+  const chunk = process.stdin.read();
+
+  while (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
