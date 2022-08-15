74b170b22868c1c3fa180cb73c3e7f9aff1d38a4
Juansu01 <thejuank0@hotmail.com>
2022-08-15 15:31:01 -0500

    Add: Using Process stdin

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  let chunk;
+  // Use a loop to make sure we read all available data.
+  while ((chunk = process.stdin.read()) !== null) {
+   process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
